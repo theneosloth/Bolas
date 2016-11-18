54d7aa3cbfa9399f189d3749242a7d976c9f4047
neosloth <superstepag@gmail.com>
2016-11-17 18:18:47 -0800

    Reverting back to the uncleaned doc file.

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,6 @@
 import discord
 import asyncio
 
-from inspect import cleandoc
-
 from .commands import CommandPlugin
 from .chat_hooks import HookPlugin
 
@@ -30,8 +28,7 @@ class Bolas(discord.Client):
 
         # Concatenate the docstrings from each one of the plugins
         self.docstring = "```{0}```".format("\n".join(
-            cleandoc(x.__doc__) for x in
-            (CommandPlugin.plugins + HookPlugin.plugins))
+            x.__doc__ for x in (CommandPlugin.plugins + HookPlugin.plugins))
         )
 
     def get_admins(self):
