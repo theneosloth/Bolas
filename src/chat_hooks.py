d9c97af46d64022d82300007620d1d5628990c50
Stefan Kuznetsov <neosloth@disroot.org>
2017-11-09 00:53:49 -0800

    Documentation update

--- src/chat_hooks.py
+++ src/chat_hooks.py
@@ -175,6 +175,7 @@ class ChannelCleaner(HookPlugin):
         self.whitelist = {
             #EDH Discord server. Remove all non link posts from #decklists
             "144547963484635137":(["decklists"], re.compile(".*http(s)*:\/\/.*")),
+            #PlayEDH
             "304276578005942272":(["decklists"], re.compile(".*http(s)*:\/\/.*")),
             #Dragon's server
             "334571063197302784 ":(["decklists"], re.compile(".*http(s)*:\/\/.*")),
