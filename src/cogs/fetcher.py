f815159f81d4eaa047ffee9ee98d8ca2bc6791f3
MelffyBunilla <mymadburner@hotmail.com>
2021-06-09 21:01:06 +0200

    Update fetcher.py

--- src/cogs/fetcher.py
+++ src/cogs/fetcher.py
@@ -2,9 +2,14 @@ import asyncio
 import re
 import requests
 import random
+import discord
+
+import urllib.parse as parse
 
 from discord import Embed
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
 from urllib.parse import quote, quote_plus
 
 from ..scryfall import ScryFall
@@ -117,7 +122,7 @@ class Fetcher(commands.Cog):
             await ctx.send(card[attr])
         else:
             await ctx.send("Not found.",delete_after=5)
-
+            
     @commands.command()
     async def art(self, ctx, *, arg=None):
         "Return the art of a given card."
@@ -194,8 +199,161 @@ class Fetcher(commands.Cog):
             k=1
         )
         card = self.sc.card_random(cute_choice[0])
-        print(card['name'])
         await self._post_card_attribute(ctx, '!"{}" include:extras'.format(card['name']), "image_art_crop")
+        
+    @commands.command()
+    async def random(self, ctx, *, arg="-t:card \(-banned:vintage or t:conspiracy or o:ante or o:'one foot' or Shahrazad\)"):
+        "Return the image of a given card at random."
+        card = self.sc.card_random(arg)
+        url = "{}/cards/search?q={}".format(self.sc.API_URL, parse.quote_plus(arg))
+        j = self.sc._load_url_as_json(url)
+        print(card['name'])
+        await ctx.send(embed=Embed(title="Probability", description="1 in {}".format(j['total_cards'])),delete_after=5)
+        await self._post_card_attribute(ctx, '!"{}" -t:token -layout:art_series -t:card \(-banned:vintage or t:conspiracy or o:ante or o:"one foot" or Shahrazad\)'.format(card['name']), "image_normal")
+        
+    @cog_ext.cog_slash(name="art",
+                      description="Return the art of a given card.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Gallia of the Endless Dance)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def art(self, ctx, *, arg=None):        
+        await self._post_card_attribute(ctx, arg, "image_art_crop")
+
+    @cog_ext.cog_slash(name="image",
+                      description="Return the image of a given card.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Void Beckoner is:godzilla)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def _image(self, ctx, *, arg=None):        
+        await self._post_card_attribute(ctx, arg, "image_normal")
+
+    @cog_ext.cog_slash(name="flavor",
+                      description="Return the flavor text of a given card.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Ancient Grudge set:isd)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def _flavor(self, ctx, *, arg=None):        
+        await self._post_card_attribute(ctx, arg, "flavor_text")
+
+    @cog_ext.cog_slash(name="reserved",
+                      description="Return whether the given card is reserved.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Raging River)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def _reserved(self, ctx, *, arg=None):
+        await self._post_card_attribute(ctx, arg, "reserved")
+
+    @cog_ext.cog_slash(name="price",
+                      description="Return the price of a given card.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Black Lotus set:lea)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def _price(self, ctx, *, arg=None):        
+        if not arg:
+            await ctx.send("Please provide a card name after the command.")
+            return
+
+        # Send a card attribute or the associated exception message
+        try:
+            card = self.sc.search_card(arg, order="usd", max_cards=1)[0]
+
+            # Any generic exception provided by scryfall
+        except ScryFall.ScryfallException as e:
+            await ctx.send(e.message, delete_after=5)
+            return
+        except ScryFall.CardLimitException as e:
+            await ctx.send(e.message, delete_after=5)
+            return
+
+        await ctx.send(card.get_price_string())
+
+    @cog_ext.cog_slash(name="rulings",
+                      description="Show all the rulings for a given card.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Name of card. (e.g.: Panglacial Wurm)",
+                              option_type=3,
+                              required=True
+                          )
+                      ])
+    async def _rulings(self, ctx, *, arg=None):        
+        if not arg:
+            await ctx.send("Please provide a card name after the command.")
+            return
+
+        try:
+            card = self.sc.card_named(arg)
+            rulings = self.sc.get_card_rulings(card.id)
+        except ScryFall.ScryfallException as e:
+            await ctx.send(e.message)
+            return
+
+        if not rulings:
+            await ctx.send("No rulings found")
+            return
+
+        # String where every line is a bold publish date followed by
+        # the comment
+        description = "\n\n".join(
+            [f'**{r["published_at"]}** {r["comment"]}' for r in rulings])
+
+        # This embed could definitely be prettier
+        await ctx.send(embed=Embed(title=card.name, description=description))
+        
+    @cog_ext.cog_slash(name="cute",
+                       description="Return the art of a cute card.")
+    async def _cute(self, ctx):        
+        cute_choice = random.choices(
+            population=["art:cute or scute", "art:emrakul", "art:kozilek", "art:ulamog"],
+            weights=[0.97, 0.01, 0.01, 0.01],
+            k=1
+        )
+        card = self.sc.card_random(cute_choice[0])
+        await self._post_card_attribute(ctx, '!"{}" include:extras'.format(card['name']), "image_art_crop")
+        
+    @cog_ext.cog_slash(name="random",
+                      description="Return the image of a given card at random.",
+                      options=[
+                          create_option(
+                              name="arg",
+                              description="Group of cards. \(e.g.: is:commander\)",
+                              option_type=3,
+                              required=False
+                          )
+                      ])
+    async def _random(self, ctx, *, arg="-t:card \(-banned:vintage or t:conspiracy or o:ante or o:'one foot' or Shahrazad\)"):
+        "Return the image of a given card at random."
+        card = self.sc.card_random(arg)
+        url = "{}/cards/search?q={}".format(self.sc.API_URL, parse.quote_plus(arg))
+        j = self.sc._load_url_as_json(url)
+        await self._post_card_attribute(ctx, '!"{}" -t:token -layout:art_series -t:card \(-banned:vintage or t:conspiracy or o:ante or o:"one foot" or Shahrazad\)'.format(card['name']), "image_normal")
+        await ctx.send(embed=Embed(title="Probability", description="1 in {}".format(j['total_cards'])),delete_after=5)
 
 def setup(bot):
     bot.add_cog(Fetcher(bot))
